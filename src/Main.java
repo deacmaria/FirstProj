04ae1343eda1515a66fd820eb64f7bdce3b71764
deacmaria <deac_maria@ymail.com>
2019-09-11 21:00:04 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Tipuri de Date
+        String rasa = "ciobanesc german";
+        String culoare = "maro";
+        byte varsta = 2;
+        float greutate = 25;
+        char gen = 'f';
+        int nivelInteligenta = 123455666;
+        long nivelEnergie = 555555555555555555L;
+        double nivelNevoieAtentie = 555555555555555555555555d;
+
+        System.out.println("Despre catelul meu");
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
